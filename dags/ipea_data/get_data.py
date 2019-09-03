83a876ccc52ec8a83d4b1d9a72e6e55afee1ba41
Leonardo Barbosa <leonardo.barbosa@keyrus.com.br>
2019-09-03 18:49:50 -0300

    Add submodule and ipea import

--- dags/ipea_data/get_data.py
+++ dags/ipea_data/get_data.py
@@ -1,7 +1,14 @@
 import airflow
 from airflow import DAG
 from datetime import datetime, timedelta
+from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
+
+import raizenlib.utils.adl as adl
+
+ADL = 'raizenprd01'
+dag_id = 'PID-retrieve_ipea_data'
+workdir = 'ldt_dev/projetos/'
 
 default_args = {
     'owner': 'Projeto IPEA Data',
@@ -12,10 +19,36 @@ default_args = {
     "email_on_retry": False
 }
 
-dag = DAG("PID-retrieve_ipea_data", catchup=False, default_args=default_args, schedule_interval="@daily",max_active_runs=1)
+dag = DAG(dag_id, catchup=False, default_args=default_args, schedule_interval="@daily", max_active_runs=1)
+
+def fn_get_metadata(**args):
+    import pyIpeaData as ipea
+    spark = adl.get_adl_spark(args["save_path"])
+    df_pd = ipea.get_metadados()
+    df = spark.createDataFrame(df_pd)
+    df.withColumn("ref_date", datetime.now().strftime("%Y-%m-%d"))
+    df.coalesce(1).write \
+        .partitionBy("ref_date") \
+        .format("parquet") \
+        .save(args["save_path"])
 
 
-retrieve_data = DummyOperator(task_id = "retrieve_data", dag = dag)
+get_metadata = PythonOperator(
+    task_id = "get_metadata",
+    python_callable = fn_get_metadata,
+    op_kwargs = {
+        'save_path': adl.adl_full_url(ADL, workdir + '/ipeadata/raw/metadados'),
+        'table_name': 'ipeadata_metadados'
+    },
+    queue = worker_queue,
+    dag = dag
+)
 
+save_metadata = DummyOperator(task_id = "save_metadata", dag = dag)
 
-retrieve_data
+get_timeseries = DummyOperator(task_id = "get_timeseries", dag = dag)
+
+save_timeseries = DummyOperator(task_id = "save_timeseries", dag = dag)
+
+retrieve_data >> [save_metadata, get_timeseries]
+get_timeseries >> save_timeseries
