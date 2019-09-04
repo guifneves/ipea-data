903ff0f2b71915161c99c67a2433197c0b273743
Leonardo Barbosa <leonardo.barbosa@keyrus.com.br>
2019-09-04 14:09:44 -0300

    IpeaData

--- dags/ipea_data/get_data.py
+++ dags/ipea_data/get_data.py
@@ -9,6 +9,7 @@ import raizenlib.utils.adl as adl
 ADL = 'raizenprd01'
 dag_id = 'PID-retrieve_ipea_data'
 workdir = 'ldt_dev/projetos/'
+worker_queue = "ipea-data-worker-queue"
 
 default_args = {
     'owner': 'Projeto IPEA Data',
@@ -22,11 +23,12 @@ default_args = {
 dag = DAG(dag_id, catchup=False, default_args=default_args, schedule_interval="@daily", max_active_runs=1)
 
 def fn_get_metadata(**args):
+    import pyspark.sql.functions as F
     import pyIpeaData as ipea
     spark = adl.get_adl_spark(args["save_path"])
     df_pd = ipea.get_metadados()
     df = spark.createDataFrame(df_pd)
-    df.withColumn("ref_date", datetime.now().strftime("%Y-%m-%d"))
+    df = df.withColumn("ref_date", F.lit(datetime.now().strftime("%Y-%m-%d")))
     df.coalesce(1).write \
         .partitionBy("ref_date") \
         .format("parquet") \
@@ -44,11 +46,27 @@ get_metadata = PythonOperator(
     dag = dag
 )
 
+def fn_save_metadata(**args):
+    print("TODO")
+
 save_metadata = DummyOperator(task_id = "save_metadata", dag = dag)
 
-get_timeseries = DummyOperator(task_id = "get_timeseries", dag = dag)
+def fn_get_timeseries(**args):
+    print("TODO")
+
+get_timeseries = PythonOperator(
+    task_id = "get_timeseries",
+    python_callable = fn_get_timeseries,
+    op_kwargs = {
+        'source_path': adl.adl_full_url(ADL, workdir + '/ipeadata/raw/metadados'),
+        'save_path': adl.adl_full_url(ADL, workdir + '/ipeadata/dados/'),
+        'table_name': 'ipeadata_metadados'
+    },
+    queue = worker_queue,
+    dag = dag
+)
 
 save_timeseries = DummyOperator(task_id = "save_timeseries", dag = dag)
 
-retrieve_data >> [save_metadata, get_timeseries]
+get_metadata >> [save_metadata, get_timeseries]
 get_timeseries >> save_timeseries
