2bfdf7b3831576c7d9af8763405f9cf207059aff
igorccsilveira1 <igor.silveira@keyrus.com.br>
2019-09-06 09:23:07 -0300

    Criação do branch

--- dags/ipea_data/get_data.py
+++ dags/ipea_data/get_data.py
@@ -36,16 +36,16 @@ def fn_get_metadata(**args):
         .save(args["save_path"])
 
 
-get_metadata = PythonOperator(
-    task_id = "get_metadata",
-    python_callable = fn_get_metadata,
-    op_kwargs = {
-        'save_path': adl.adl_full_url(ADL, workdir + '/ipea_data/raw/metadados'),
-        'table_name': 'ipeadata_metadados'
-    },
-    queue = worker_queue,
-    dag = dag
-)
+# get_metadata = PythonOperator(
+#     task_id = "get_metadata",
+#     python_callable = fn_get_metadata,
+#     op_kwargs = {
+#         'save_path': adl.adl_full_url(ADL, workdir + '/ipea_data/raw_igor/metadados'),
+#         'table_name': 'ipeadata_metadados'
+#     },
+#     queue = worker_queue,
+#     dag = dag
+# )
 
 def fn_save_metadata(**args):
     print("TODO")
@@ -64,32 +64,43 @@ save_metadata = PythonOperator(
 def fn_get_timeseries(**args):
     import pyspark.sql.functions as F
     import pyIpeaData as ipea
+    import pandas as pd
+    from datetime import date
+    import raizenlib.utils.adl as adl
+    import multiprocessing as mp
 
     current_date = datetime.now().strftime("%Y-%m-%d")
     spark = adl.get_adl_spark(args["save_path"])
-    df_md = spark.read.format("parquet").load(args["source_path"]).filter(F.col("ref_date") == current_date)
-    #codes = list(map(lambda i: i.SERCODIGO, df_md.select("SERCODIGO").distinct().collect()))
+    #df_md = spark.read.format("parquet").load(args["source_path"]).filter(F.col("ref_date") == current_date)
+    df_md = spark.read.format("parquet").load(args["source_path"])
+    codes = list(map(lambda i: i.SERCODIGO, df_md.select("SERCODIGO").distinct().collect()))
 
-    count = 0
+    client, path = adl.get_adl_client(args["save_path"])
+     
+    # pool = mp.Pool(mp.cpu_count())
+    # [pool.apply(fn_save_on_data_lake, args=(client,path,x)) for x in codes]    
+    # pool.close()     
 
-    for code in codes:
-        df_pd = ipea.get_serie(code)
-        df = spark.createDataFrame(df_pd)
-        df = df.withColumn("ref_date", F.lit(datetime.now().strftime("%Y-%m-%d")))
-        count += df.count()
-        df.coalesce(1).write \
-            .partitionBy("ref_date") \
-            .format("parquet") \
-            .save(args["save_path"] + "/" + code)
+    list(map(lambda x: fn_save_on_data_lake(client, path, x), codes))         
 
-    print("*" * 80, count)
+    #code = 'ABATE_ABPEAV'
+
+
+def fn_save_on_data_lake(client,save_path, code):
+    import pyIpeaData as ipea
+    import pandas as pd
+    from datetime import date
+
+    df_pd = ipea.get_serie(code)    
+    with client.open(save_path + "ref_date=" + date.today().strftime("%Y-%m-%d") + "/" + code + ".parquet", 'wb') as f:        
+        df_pd.to_parquet(f)
 
 get_timeseries = PythonOperator(
     task_id = "get_timeseries",
     python_callable = fn_get_timeseries,
     op_kwargs = {
-        'source_path': adl.adl_full_url(ADL, workdir + '/ipea_data/raw/metadados'),
-        'save_path': adl.adl_full_url(ADL, workdir + '/ipea_data/dados/'),
+        'source_path': adl.adl_full_url(ADL, workdir + '/ipea_data/raw_igor//metadados/'),
+        'save_path': adl.adl_full_url(ADL, workdir + 'ipea_data/dados_igor/'),
         'table_name': 'ipeadata_metadados'
     },
     queue = worker_queue,
@@ -110,5 +121,6 @@ save_timeseries = PythonOperator(
     dag = dag
 )
 
-get_metadata >> [save_metadata, get_timeseries]
+#get_metadata >> 
+[save_metadata, get_timeseries]
 get_timeseries >> save_timeseries
