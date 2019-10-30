be91ae32effce802068d72d64f68cf5c0d18570d
igorccsilveira1 <igor.silveira@keyrus.com.br>
2019-10-30 10:56:36 -0300

    paralelismo

--- dags/ipea_data/tasks/ingest.py
+++ dags/ipea_data/tasks/ingest.py
@@ -30,21 +30,28 @@ def save_metadata(**args):
     #if df.count() > 0:
     #    df.write.jdbc(url=jdbc_url, table=args["table_name"], mode='overwrite', properties=conn_properties)
 
-def get_from_ipea(code):
-    import pyIpeaData as ipea
-    print("==========", code)
-    return ipea.get_serie(code[1])
+# def get_from_ipea(code):
+#     import pyIpeaData as ipea
+#     print("==========", code)
+#     return ipea.get_serie(code[1])
 
 def get_timeseries(**args):
     import pyspark.sql.functions as F
     import pyIpeaData as ipea
+
+    def get_from_ipea(code):
+        import pyIpeaData as ipea
+        print("==========", code)
+        return ipea.get_serie(code[1])
 
     current_date = datetime.now().strftime("%Y-%m-%d")
     spark = adl.get_adl_spark(args["save_path"])
     df_md = spark.read.format("parquet").load(args["source_path"]).filter(F.col("ref_date") == current_date)
     codes = list(map(lambda i: i.SERCODIGO, df_md.select("SERCODIGO").distinct().collect()))
 
-    dfs = list(map(lambda code: get_from_ipea(code), list(enumerate(codes))))
+    print(codes)
+    #dfs = list(map(lambda code: get_from_ipea(code), list(enumerate(codes))))    
+    dfs = spark.sparkContext.parallelize(list(enumerate(codes))).map(lambda x: get_from_ipea(x)).collect()
 
     df_pd = pd.concat(dfs, ignore_index = True)
 
@@ -53,7 +60,7 @@ def get_timeseries(**args):
     df = df.withColumn("ref_date", F.lit(datetime.now().strftime("%Y-%m-%d")))
     df.write \
         .partitionBy("SERCODIGO", "ref_date") \
-        .parquet(args["save_path"], mode = "append")
+        .parquet(args["save_path"], mode = "append")    
 
 def save_timeseries(**args):
     import pyspark.sql.functions as F
