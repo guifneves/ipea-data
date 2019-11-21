347b5944e909fb3abfa12ab7343a0985d8e22d8b
igorccsilveira1 <igor.silveira@keyrus.com.br>
2019-11-21 17:36:47 -0300

    Tabelas auxiliares

--- dags/ipea_data/pid-retrieve-metadata.py
+++ dags/ipea_data/pid-retrieve-metadata.py
@@ -31,9 +31,10 @@ dag = DAG(
     concurrency = 4
 )
 
-get_metadata = PythonOperator(
-    task_id = "get_metadata",
-    python_callable = ingest.get_metadata,
+## Metadados
+get_metadados = PythonOperator(
+    task_id = "get_metadados",
+    python_callable = ingest.get_metadados,
     op_kwargs = {
         'save_path': adl.adl_full_url(ADL, workdir + '/trusted/ipea_data/metadados/')
     },
@@ -41,12 +42,100 @@ get_metadata = PythonOperator(
     dag = dag
 )
 
-save_metadata = PythonOperator(
-    task_id = "save_metadata",
-    python_callable = ingest.save_metadata,
+save_metadados = PythonOperator(
+    task_id = "save_metadados",
+    python_callable = ingest.save_metadados,
     op_kwargs = {
         'source_path': adl.adl_full_url(ADL, workdir + '/trusted/ipea_data/metadados/'),
         'table_name': 'metadados'
+    },
+    queue = worker_queue,
+    dag = dag
+)
+
+## Territorios
+get_territorios = PythonOperator(
+    task_id = "get_territorios",
+    python_callable = ingest.get_territorios,
+    op_kwargs = {
+        'save_path': adl.adl_full_url(ADL, workdir + '/trusted/ipea_data/territorios/')
+    },
+    queue = worker_queue,
+    dag = dag
+)
+
+save_territorios = PythonOperator(
+    task_id = "save_territorios",
+    python_callable = ingest.save_territorios,
+    op_kwargs = {
+        'source_path': adl.adl_full_url(ADL, workdir + '/trusted/ipea_data/territorios/'),
+        'table_name': 'territorios'
+    },
+    queue = worker_queue,
+    dag = dag
+)
+
+## Temas
+get_temas = PythonOperator(
+    task_id = "get_temas",
+    python_callable = ingest.get_temas,
+    op_kwargs = {
+        'save_path': adl.adl_full_url(ADL, workdir + '/trusted/ipea_data/temas/')
+    },
+    queue = worker_queue,
+    dag = dag
+)
+
+save_temas = PythonOperator(
+    task_id = "save_temas",
+    python_callable = ingest.save_temas,
+    op_kwargs = {
+        'source_path': adl.adl_full_url(ADL, workdir + '/trusted/ipea_data/temas/'),
+        'table_name': 'temas'
+    },
+    queue = worker_queue,
+    dag = dag
+)
+
+## Paises
+get_paises = PythonOperator(
+    task_id = "get_paises",
+    python_callable = ingest.get_paises,
+    op_kwargs = {
+        'save_path': adl.adl_full_url(ADL, workdir + '/trusted/ipea_data/paises/')
+    },
+    queue = worker_queue,
+    dag = dag
+)
+
+save_paises = PythonOperator(
+    task_id = "save_paises",
+    python_callable = ingest.save_paises,
+    op_kwargs = {
+        'source_path': adl.adl_full_url(ADL, workdir + '/trusted/ipea_data/paises/'),
+        'table_name': 'paises'
+    },
+    queue = worker_queue,
+    dag = dag
+)
+
+## Fontes
+get_fontes = PythonOperator(
+    task_id = "get_fontes",
+    python_callable = ingest.get_fontes,
+    op_kwargs = {
+        'save_path': adl.adl_full_url(ADL, workdir + '/trusted/ipea_data/fontes/')
+    },
+    queue = worker_queue,
+    dag = dag
+)
+
+save_fontes = PythonOperator(
+    task_id = "save_fontes",
+    python_callable = ingest.save_fontes,
+    op_kwargs = {
+        'source_path': adl.adl_full_url(ADL, workdir + '/trusted/ipea_data/fontes/'),
+        'table_name': 'fontes'
     },
     queue = worker_queue,
     dag = dag
@@ -62,16 +151,10 @@ clear_timeseries = PythonOperator(
     dag = dag
 )
 
-save_relationship_table  = PythonOperator(
-    task_id = "save_relationship_table",
-    python_callable = ingest.save_relationship_table,
-    op_kwargs = {
-        'source_path': adl.adl_full_url(ADL, workdir + '/trusted/ipea_data/metadados/'),
+get_metadados >> save_metadados 
+get_territorios >> save_territorios 
+get_temas >> save_temas 
+get_paises >> save_paises 
+get_fontes >> save_fontes
+clear_timeseries
 
-        'table': 'sercodigo_temcodigo'
-    },
-    queue = worker_queue,
-    dag = dag
-)
-
-get_metadata >> save_metadata >> save_relationship_table >> clear_timeseries
